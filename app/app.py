25de8f34045583b255089b2056a9ad822dda95d1
camiloRueda <crcruedaal@unal.edu.co>
2018-10-04 00:58:44 -0500

    CRUD rest completo

--- app/app.py
+++ app/app.py
@@ -5,6 +5,7 @@ from flask import abort
 from utils import *
 from bson import BSON
 from bson import json_util
+from flask import request
 import json
 
 def init():
@@ -13,7 +14,7 @@ def init():
 
 app = Flask(__name__)
 
-#--------------------------------GET ------------------------------------------
+#--------------------------------Violence events ------------------------------------------
 
 @app.route('/analysis_data/violence_events/', methods=['GET'])
 def get_violence_events():
@@ -23,16 +24,43 @@ def get_violence_events():
 		doc.append(i)
 	return json.dumps(doc, sort_keys=True, indent=4, default=json_util.default)
 
-@app.route('/analysis_data/violence_events/<int:event_id>', methods=['GET'])
+@app.route('/analysis_data/violence_events/<int:event_id>', methods=['GET','PUT','DELETE'])
 def get_violence_event(event_id):
-	try:
-		violence_event=client_mongo.analysis_data.violence_events.find({"_id":event_id})
-	except:
-		print ("id {} not founded".format(event_id))
-		abort(404)
-		return 0
-	return json.dumps(violence_event[0], sort_keys=True, indent=4, default=json_util.default)
-
+	if request.method == 'GET':
+		try:
+			violence_event=client_mongo.analysis_data.violence_events.find({"_id":event_id})
+		except:
+			print ("id {} not founded".format(event_id))
+			abort(404)
+			return 400
+		return json.dumps(violence_event[0], sort_keys=True, indent=4, default=json_util.default)
+	elif request.method == 'PUT':
+	    data = {
+			"_id": event_id,
+			"vio_type":request.json['type'],
+			"vio_place": request.json['place'],
+			"vio_victime_id": request.json['victime_id']
+	    	}
+	    client_mongo.analysis_data.violence_events.update({"_id":event_id},{ "vio_victime_id" : data["vio_victime_id"], \
+		 "vio_place" : data["vio_place"],\
+		 "vio_type" : data["vio_type"] })
+	    return jsonify({'data': data}),201
+	else:
+		client_mongo.analysis_data.violence_events.remove({"_id":event_id})
+		return "event {} succesfully deleted".format(event_id),201 
+#http://0.0.0.0:5000/analysis_data/violence_events/
+@app.route('/analysis_data/violence_events/', methods=['POST'])
+def create_violence_event():
+    if not request.json or not 'id' in request.json:
+        abort(400)
+    data = {
+		"_id": request.json['id'],
+		"vio_type":request.json['type'],
+		"vio_place": request.json['place'],
+		"vio_victime_id": request.json['victime_id']
+    	}
+    client_mongo.analysis_data.violence_events.insert(data)
+    return jsonify({'data': data}),201
 
 @app.errorhandler(404)
 def not_found(error):
