0f94e981b2b21b40ad1c818eb65e5bd99ff80e9d
camiloRueda <crcruedaal@unal.edu.co>
2018-10-04 01:21:36 -0500

    servicio completo

--- app/utils.py
+++ app/utils.py
@@ -1,8 +1,7 @@
 from pymongo import MongoClient
 
 def get_mongo_instance():
-	connection_str = 'mongodb://root:example@192.168.99.102:27017/analysis_data'
-    #connection_str = 'mongodb://admin:1234@localhost:27017/analysis_data'
-    client = MongoClient(connection_str)
-    return client
+	connection_str = 'mongodb://root:example@192.168.99.102:27017/analysis_data'#connection_str = 'mongodb://admin:1234@localhost:27017/analysis_data'
+	client = MongoClient(connection_str)
+	return client
 #eval $(docker-machine env rancher-node2)
